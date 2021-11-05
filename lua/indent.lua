4f230008025a0626a9c1ef685fe9a00f4747a203
lee-shun <lee970802@163.com>
2021-11-05 10:36:05 -0400

    u

--- lua/indent.lua
+++ lua/indent.lua
@@ -10,3 +10,4 @@ vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_context_highlight = "Label"
 vim.g.indent_blankline_context_patterns = {"class", "function", "method", "^if", "^while", "^for", "^object", "^table", "block"}
 vim.g.indent_blankline_show_trailing_blankline_indent = false
+-- vim.g.indent_blankline_show_current_context_start = true
