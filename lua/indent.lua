a25661115f836cbe9edd7f7e9e710e4f42570289
lee-shun <lee970802@163.com>
2021-11-06 17:26:00 -0400

    update

--- lua/indent.lua
+++ lua/indent.lua
@@ -1,8 +1,9 @@
+require("indent_blankline").setup {}
 require("indent_blankline").setup {
     char = "┆",
     show_current_context = true,
     show_current_context_start = true,
-    context_patterns = {"class", "function", "method", "^if", "^while", "^for", "^object", "^table", "block"},
+    context_patterns = {"class", "function", "method", "^if", "^else","^else if", "^while", "^for", "^object", "^table", "block"},
     show_end_of_line = false,
     filetype_exclude = {"help", "Nvimtree", "coc-explorer", "dashboard"},
     buftype_exclude = {"terminal", "prompt"},
