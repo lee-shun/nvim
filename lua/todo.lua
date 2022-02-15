f4c709ca26962aff780218c49b55a87669aefede
lee-shun <lee970802@163.com>
2022-02-15 11:27:41 -0500

    u

--- lua/todo.lua
+++ lua/todo.lua
@@ -1,16 +1,36 @@
-  require("todo-comments").setup {
-  keywords = {
-    FIX = {
-      icon = "", -- icon used for the sign, and in search results
-      color = "error", -- can be a hex color, or a named color (see below)
-      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-      -- signs = false, -- configure signs for some keywords individually
+require("todo-comments").setup {
+    highlight = {
+        before = "", -- "fg" or "bg" or empty
+        keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+        after = "fg", -- "fg" or "bg" or empty
+        pattern = [[.*<(KEYWORDS)(\([^\)]*\))?:]],
+        comments_only = true, -- uses treesitter to match keywords in comments only
+        max_line_len = 400, -- ignore lines longer than this
+        exclude = {}, -- list of file types to exclude highlighting
     },
-    TODO = { icon = "", color = "info" },
-    HACK = { icon = "", color = "warning" },
-    WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
-    PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-    NOTE = { icon = "", color = "hint", alt = { "INFO" } },
-    STEP = { icon = "", color = "#BB8FCE", alt = { "Step" } },
-  },
-  }
+    search = {
+        command = "rg",
+        args = {
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+        },
+        pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]],
+    },
+    keywords = {
+        FIX = {
+            icon = "", -- icon used for the sign, and in search results
+            color = "error", -- can be a hex color, or a named color (see below)
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+        },
+        TODO = { icon = "", color = "info" },
+        HACK = { icon = "", color = "warning" },
+        WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = "", color = "hint", alt = { "INFO" } },
+        STEP = { icon = "", color = "#BB8FCE", alt = { "Step" } },
+    },
+}
